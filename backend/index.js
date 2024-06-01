8dd10bff9d82af42274b6e90e60e5ea7d088f38b
himav95 <himaviamarakoon@gmail.com>
2024-06-01 18:16:43 +0530

    Backend: implemented Controllers/PaymentControllere.js and Routes/PaymentRoutes. Modified index.js regarding payment gateway.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const PaymentRoutes = require('./PaymentRoutes');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+// Mount the payment routes.
+app.use('/api/payments', Payme);
+
+
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+
+
